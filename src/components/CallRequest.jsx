010b1cc7e021e71641ac99a2c5e4612afe60d993
Eugene <evgeha_ivanov_01@bk.ru>
2022-03-24 17:41:32 +0300

    17:41 24.03.2022

--- src/components/CallRequest.jsx
+++ src/components/CallRequest.jsx
@@ -1,10 +1,46 @@
 import './CallRequest.scss'
 import $ from 'jquery'
+import { useEffect } from 'react'
 const CallRequest = () =>{
 
-    console.log($('.input_field, .input_holder label').height())
+    useEffect(async ()=>{
+        $(window).resize(()=>{
+            if($(window).width()<=991){
+                $('.input_field, .input_holder label').css('margin-left', -$('.input_field, .input_holder label').width()/2)
+                $('.input_field, .input_holder label').css('margin-top', -$('.input_field, .input_holder label').height()/2)
+
+                $('.input_field, .input_holder label').css('left', '50%')
+                $('.input_field, .input_holder label').css('top', '50%')
+            }
+            
+            if($(window).width()>991){
+                $('.input_field, .input_holder label').css('margin', '0px 0px 5px 1px')
+
+                $('.input_field, .input_holder label').css('left', '0')
+                $('.input_field, .input_holder label').css('top', '0')
+            }
+        })
+
+        $(window).ready(()=>{
+            if($(window).width()<=991){
+                $('.input_field, .input_holder label').css('margin-left', -$('.input_field, .input_holder label').width()/2)
+                $('.input_field, .input_holder label').css('margin-top', -$('.input_field, .input_holder label').height()/2)
+
+                $('.input_field, .input_holder label').css('left', '50%')
+                $('.input_field, .input_holder label').css('top', '50%')
+            }
+            
+            if($(window).width()>991){
+                $('.input_field, .input_holder label').css('margin', '0px 0px 5px 1px')
+
+                $('.input_field, .input_holder label').css('left', '0')
+                $('.input_field, .input_holder label').css('top', '0')
+            }
+        })
+    }, [])
+    //console.log($('.input_field, .input_holder label').height())
     return(
-        <div className="call_request_holder mt-5 container pb-2">
+        <div className="call_request_holder mt-5 container pb-2 col-md-8">
             <div className="row justify-content-center">
                 <span className="request_text">Оставьте заявку на звонок и я сам Вам перезвоню!</span>
 
