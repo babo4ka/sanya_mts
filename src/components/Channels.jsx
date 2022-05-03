b79368cc1290a52478a99b147b0ed73090a33505
Eugene <evgeha_ivanov_01@bk.ru>
2022-05-03 21:28:45 +0300

    убрал лишние строки

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,33 +1,8 @@
-import { useState } from 'react'
 import './Channels.scss'
-import $ from 'jquery'
 import { Link } from 'react-router-dom'
-const channels = require('../channels.json')
 const ch_list = require('../список_каналов.pdf')
+
 const Channels = () =>{
-    
-
-    const [group, setGroup] = useState(0)
-
-    const goNext = () =>{
-        $('#channels').fadeToggle('fast', ()=>{
-            setGroup(prev => {
-                if(prev + 1 == channels.channels.length)return 0
-                return prev+1
-            })
-        })
-        $('#channels').fadeToggle('fast')
-    }
-
-    const goPrev = () =>{
-        $('#channels').fadeToggle('fast', ()=>{
-            setGroup(prev => {
-                if(prev - 1 < 0)return channels.channels.length-1
-                return prev-1
-            })
-        })
-        $('#channels').fadeToggle('fast')
-    }
     
     return(
         <div id="channels" className="container mt-5">
